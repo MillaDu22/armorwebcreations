8ae6d7339bb0aebd50647fb8453c5f4896f13dfb
Milla22 <marti.ludmilla@orange.fr>
2025-05-15 22:28:28 +0200

    Ajout d'images page home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,20 @@
-import Navbar from "../../components/Navbar/index";
-import "./header.css";
+import Screens from '../../assets/images/screens.webp';
+import Logo3Video from '../Logo3/index.jsx';
+import './header.css';
 
 function Header() {
-    return(
-        <header className="container-header">
-            <Navbar />
+    return (
+        <header className="container-hero">
+            <div className="box-hero">
+                <div className="container-txt">
+                    <Logo3Video />
+                </div>
+                <div className="container-screens">
+                    <img className="asset-hero" src={Screens} alt="preview responsive websites" />
+                </div>
+            </div>
         </header>
-    )
+    );
 }
+
 export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Header from '../src/components/Header/index.jsx';
+import Navbar from '../src/components/Navbar/index.jsx';
 import Footer from "../src/components/Footer/index.jsx";
 import Router from "./Router/router.jsx";
 import { HashRouter } from "react-router-dom";
@@ -13,7 +13,7 @@ import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
-      <Header />
+      <Navbar />
       <Router />
       <Footer />
     </HashRouter>
